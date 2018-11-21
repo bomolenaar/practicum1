8546862c5cba32a04546baf8c7ac550c3bdf69f1
bomolenaar <45235011+bomolenaar@users.noreply.github.com>
2018-11-21 20:08:20 +0100

    Add files via upload

--- Vet leuke opdracht.cs
+++ Vet leuke opdracht.cs
@@ -2,14 +2,10 @@
 using System.Drawing;
 using System.Windows.Forms;
 using System.Threading;
-using System.Diagnostics;
-
 
 namespace Hallo
 {
     class Help
-
-
     {
         static void Main()
         {
@@ -22,21 +18,6 @@ namespace Hallo
     //liefst hier nog toevoegen dat de knoppen in eerste instantie automatisch centreren ongeacht schermformaat (anders laten zoals het is - berekend op een 1920x1080px scherm)
     class Scherm : Form
     {
-        public DateTime Starttijd;
-        public DateTime Reactietijd;
-
-        //Random Number Generator
-        public void RNG()
-        {
-            Random rnd = new Random();
-            int r1 = rnd.Next(1870);
-            Thread.Sleep(9);
-            int r2 = rnd.Next(1030);
-            Thread.Sleep(3);
-            int r3 = rnd.Next(30, 500);
-            Thread.Sleep(17);
-            int r4 = rnd.Next(30, 500);
-        }
         //Form method
         public Scherm()
         {
@@ -46,7 +27,16 @@ namespace Hallo
 
             //Locaties
             Point startplek; Point reactieplek;
-            startplek = new Point(910, 490); reactieplek = new Point(400, 200);
+            startplek = new Point(910, 490);
+            reactieplek = new Point(400, 200);
+
+            //Random Number Generator
+            Random rnd = new Random();
+            int r1 = rnd.Next(1870);
+            Thread.Sleep(9);
+            int r2 = rnd.Next(1030);
+            Thread.Sleep(3);
+            int r3 = rnd.Next(30, 500);
 
             //'Start' knop
             Button start;
@@ -67,7 +57,7 @@ namespace Hallo
                 Font = new Font("Comic Sans MS", 20),
                 Location = reactieplek,
                 BackColor = Color.HotPink,
-                Size = new Size(100, 100),
+                Size = new Size(r3, r3),
                 Visible = false,
             };
 
@@ -76,31 +66,26 @@ namespace Hallo
             start.Click += start_klik;
             reactie.Click += reactie_klik;
 
-
-
-
             //daadwerkelijke functionaliteit van de knoppen          
             void start_klik(object sender, EventArgs e)
             {
                 start.Visible = false;
                 reactie.Visible = true;
-                DateTime Starttijd = DateTime.Now;
-
-
             }
 
             void reactie_klik(object sender, EventArgs e)
             {
                 start.Visible = true;
                 reactie.Visible = false;
-                reactie.Size = new Size(200, 200);
-                DateTime Reactietijd = DateTime.Now;
-                long elapsedTicks = Reactietijd.Ticks - Starttijd.Ticks;
-                TimeSpan elapsedSpan = new TimeSpan(elapsedTicks);
-                Console.WriteLine(elapsedSpan);
+                int r4 = rnd.Next(80, 500);
+                reactie.Size = new Size(r4, r4);
+                Thread.Sleep(5);
+                int r5 = rnd.Next(1870);
+                Thread.Sleep(11);
+                int r6 = rnd.Next(1030);
+                reactie.Location = new Point(r5, r6);
             }
+
         }
-
     }
-
 }
