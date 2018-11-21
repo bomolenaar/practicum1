02867be66055e92cf5181e2232aa43728cbe495d
bomolenaar <45235011+bomolenaar@users.noreply.github.com>
2018-11-21 15:09:04 +0100

    Eerste werkende versie met groot scherm
    Hier doen de knoppen nog niks maar de form is iig in orde. Het zou chill zijn als we ook de knoppen automatisch zouden kunnen centreren oid. En we moeten natuurlijk het event deel gan schrijven.

---
+++ Vet leuke opdracht.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Hallo
+{
+    class Help
+    {
+        static void Main()
+        {
+            Scherm scherm; //kennelijk moest dit dus hier staan???
+            scherm = new Scherm();
+            Application.Run(new Scherm());
+        }
+    }
+}
+
+//Form
+class Scherm : Form
+{
+    public Scherm()
+    {
+        this.Text = "Fitts' Law";
+        this.BackColor = Color.CadetBlue;
+        this.WindowState = FormWindowState.Maximized;
+       
+//Locaties
+        Point startplek; Point beginplek;
+        startplek = new Point(200, 200); beginplek = new Point(400, 200);
+
+//'Start' knop
+        Button start;
+        start = new Button
+        {
+            Text = "Click here",
+            Font = new Font("Comic Sans MS", 20),
+            Location = startplek,
+            BackColor = Color.BlueViolet,
+            Size = new Size(100, 100),
+        };
+
+//'Begin' knop
+        Button begin;
+        begin = new Button
+        {
+            Text = "Click here",
+            Font= new Font("Comic Sans MS", 20),
+            Location = beginplek,
+            BackColor = Color.HotPink,
+            Size = new Size(100, 100),
+        };
+        
+        this.Controls.Add(start);
+        this.Controls.Add(begin);
+    }
+}
