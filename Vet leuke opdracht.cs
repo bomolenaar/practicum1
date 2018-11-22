1c4a700306d9dd2916b7c3b61a0f5efca34ae9a3
bomolenaar <45235011+bomolenaar@users.noreply.github.com>
2018-11-22 15:27:41 +0100

    Add files via upload
    letterlijk alles werkt behalve die knop

--- Vet leuke opdracht.cs
+++ Vet leuke opdracht.cs
@@ -24,7 +24,9 @@ namespace Practicum1
         public Button reactie; Button start;
         public Random rnd;
         public Point startplek; Point reactieplek1; Point reactieplek2; Point muisplek;
-        public int r3;
+        public Graphics g;
+        public double dpi, dx, dy, dpx, d;
+        public int r1, r2, r3;
 
         //Form method
         public Scherm()
@@ -33,16 +35,20 @@ namespace Practicum1
             this.BackColor = Color.CadetBlue;
             this.WindowState = FormWindowState.Maximized;
 
+            //D
+            g = this.CreateGraphics();
+            dpi = g.DpiX;
+           
             //Random Number Generator
             rnd = new Random();
-            int r1 = rnd.Next(1870);
+            r1 = rnd.Next(1870);
             Thread.Sleep(9);
-            int r2 = rnd.Next(1030);
+            r2 = rnd.Next(1030);
             Thread.Sleep(3);
             r3 = rnd.Next(30, 500);
 
             //Locaties
-            startplek = new Point(300,100);
+            startplek = new Point(300, 100);
             reactieplek1 = new Point(r1, r2);
 
             //'Start' knop
@@ -50,7 +56,7 @@ namespace Practicum1
             {
                 Text = "Click here",
                 Font = new Font("Comic Sans MS", 20),
-                Location = startplek,
+                Location = startplek,                                   // FIX KNOP FIX KNOP FIX KNOP FIX KNOP FIX KNOP FIX KNOP FIX KNOP FIX KNOP FIX KNOP FIX KNOP FIX KNOP FIX KNOP FIX KNOP FIX KNOP FIX KNOP FIX KNOP FIX KNOP FIX KNOP 
                 BackColor = Color.BlueViolet,
                 Size = new Size(100, 100),
                 Visible = true,
@@ -89,22 +95,23 @@ namespace Practicum1
             start.Visible = !start.Visible;
             reactie.Visible = !reactie.Visible;
 
-            int r4 = rnd.Next(80, 500);
-            reactie.Size = new Size(r4, r4);
+            r3 = rnd.Next(80, 500);
+            reactie.Size = new Size(r3, r3);
             Thread.Sleep(5);
-            int r5 = rnd.Next(1870);
+            r1 = rnd.Next(1870);
             Thread.Sleep(11);
-            int r6 = rnd.Next(1030);
-            reactieplek2 = new Point(r5, r6);
+            r2 = rnd.Next(1030);
+            reactieplek2 = new Point(r1, r2);
             reactie.Location = reactieplek2;
 
-            double dx = reactieplek2.X - muisplek.X;
-            double dy = reactieplek2.Y - muisplek.Y;
-            double dpx = Math.Sqrt((dx * dx) + (dy * dy));
-            Console.WriteLine("D: " + dpx);
- 
+            dx = reactieplek2.X - muisplek.X;
+            dy = reactieplek2.Y - muisplek.Y;
+            dpx = Math.Sqrt((dx * dx) + (dy * dy));
+            d = (dpx / dpi) * 2.54;
+            Console.WriteLine("D: " + d);
+
             Console.WriteLine("W: " + r3);
-            Console.WriteLine("ID: " + Math.Log((2*dpx / r3), 2.0));
+            Console.WriteLine("ID: " + Math.Log((2 * d / r3), 2.0));
             Console.WriteLine("T: ");
 
             Reactietijd = DateTime.Now;
